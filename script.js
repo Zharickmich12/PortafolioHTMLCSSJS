95e2004374a018c316ff7140ec854b225d3bd760
Zharick Michelle <128750315+Zharickmich12@users.noreply.github.com>
2024-02-10 17:04:12 -0500

    name

--- script.js
+++ script.js
@@ -29,6 +29,28 @@ let header =document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
 
-    
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 
 };
+
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+};
+
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
